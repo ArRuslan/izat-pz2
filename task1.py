aaba271d00efbd4028159fd7b3d83c00ce1c6c45
RuslanUC <dev_ruslan_uc@protonmail.com>
2026-02-27 18:23:03 +0200

    refactor task 1 a little bit

--- task1.py
+++ task1.py
@@ -1,21 +1,42 @@
 from openpyxl import load_workbook
+from openpyxl.cell import Cell
 from openpyxl.styles import PatternFill
 
-fill_green = PatternFill(
-    start_color="8000FF00",
-    end_color="8000FF00",
-    fill_type="solid",
-)
-fill_blue = PatternFill(
-    start_color="800000FF",
-    end_color="800000FF",
-    fill_type="solid",
-)
-fill_yellow = PatternFill(
-    start_color="80FFFF00",
-    end_color="80FFFF00",
-    fill_type="solid",
-)
+green = "8000FF00"
+blue = "800000FF"
+yellow = "80FFFF00"
+
+fill_green = PatternFill(start_color=green, end_color=green, fill_type="solid")
+fill_blue = PatternFill(start_color=blue, end_color=blue, fill_type="solid")
+fill_yellow = PatternFill(start_color=yellow, end_color=yellow, fill_type="solid")
+
+
+def _process_cell(cell: Cell) -> None:
+    if not isinstance(cell.value, (int, str)):
+        return
+
+    if isinstance(cell.value, int):
+        cell.fill = fill_green
+        return
+
+    if cell.value.isdigit():
+        cell.fill = fill_green
+        return
+
+    chars = False
+    digits = False
+    for char in cell.value:
+        if char.isdigit():
+            digits = True
+        else:
+            chars = True
+
+    if chars and digits:
+        cell.fill = fill_yellow
+    elif chars:
+        cell.fill = fill_blue
+    elif digits:
+        cell.fill = fill_green
 
 
 def main():
@@ -24,28 +45,7 @@ def main():
 
     for row in ws:
         for col in row:
-            if col.value is None:
-                continue
-            if isinstance(col.value, int):
-                col.fill = fill_green
-            elif isinstance(col.value, str):
-                if col.value.isdigit():
-                    col.fill = fill_green
-                else:
-                    chars = False
-                    digits = False
-                    for char in col.value:
-                        if char.isdigit():
-                            digits = True
-                        else:
-                            chars = True
-
-                    if chars and digits:
-                        col.fill = fill_yellow
-                    elif chars:
-                        col.fill = fill_blue
-                    elif digits:
-                        col.fill = fill_green
+            _process_cell(col)
 
     wb.save("pz1-processed.xlsx")
 
